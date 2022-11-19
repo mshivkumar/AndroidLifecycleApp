6e95fcd7b04f8f71ea447d633c74c705b97292b1
Shiv Maurya <shiv@MacBook-Pro.local>
2022-11-19 19:45:08 +0530

    initial commit

---
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.firstandroidapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ lib/src/main/java/com/example/lib/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.lib;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Kya mast App hai!");
+    }
+}
