072214f450ce62d3a2131c61afbe14766d5cfda0
Shiv Maurya <shiv@MacBook-Pro.local>
2022-11-20 01:53:48 +0530

    navigating to browser on button lcivk

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -1,25 +1,34 @@
 package com.example.firstandroidapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titleTextView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://www.google.com"));
+                startActivity(i);
+            }
+        });
+
+
     }
 }
