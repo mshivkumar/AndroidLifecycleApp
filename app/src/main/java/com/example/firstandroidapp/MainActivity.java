9d47e7a7abdc3177a1c0c916c7f8d0168ca1b988
Shiv Maurya <shiv@MacBook-Pro.local>
2022-12-03 15:39:58 +0530

    addec checkbox

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,47 +18,31 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    CheckBox checkBoxOne;
+    CheckBox checkBoxTwo;
+    Button checkButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+        checkBoxOne = findViewById(R.id.checkbox_one);
+        checkBoxTwo = findViewById(R.id.checkbox_two);
+        checkButton = findViewById(R.id.check_button);
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBoxOne.isChecked()) {
+                    Toast.makeText(MainActivity.this, "CheckBox One Checked", Toast.LENGTH_SHORT).show();
+                }
+                if(checkBoxTwo.isChecked()) {
+                    Toast.makeText(MainActivity.this, "CheckBox Two Checked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Toast.makeText(this, "onStop()", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Toast.makeText(this, "onDestroy()", Toast.LENGTH_LONG).show();
-    }
 }
