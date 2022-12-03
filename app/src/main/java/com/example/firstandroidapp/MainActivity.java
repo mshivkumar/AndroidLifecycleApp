19fd9441c83ecdf9b502f03b44622001b268a15e
Shiv Maurya <shiv@MacBook-Pro.local>
2022-12-03 14:06:35 +0530

    added gradient as background

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -22,42 +22,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Toast.makeText(this, "onStop()", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Toast.makeText(this, "onDestroy()", Toast.LENGTH_LONG).show();
     }
 }
